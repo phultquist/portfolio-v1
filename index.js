e210f1124189d9fcca27b58f1b96c6b369c815f4
phultquist <phultquist@imsa.edu>
2020-07-27 15:58:48 -0500

    dynamic

---
+++ index.js
@@ -0,0 +1,10 @@
+const express = require('express');
+const app = express();
+
+const path = require('path');
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public/home.html')); //write a response to the client
+})
+
+app.listen(8080);
