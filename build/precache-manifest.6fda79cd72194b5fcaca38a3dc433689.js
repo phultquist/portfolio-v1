dff6580d8ae1fe144fe5a9a47fdb44326c11b0f2
phultquist <phultquist@imsa.edu>
2020-10-03 08:55:26 -0500

    stuff

---
+++ build/precache-manifest.6fda79cd72194b5fcaca38a3dc433689.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7d800c2b12b488ef6d1baf67b50b6e61",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c7ab8d91c43b0bc410d6",
+    "url": "/static/css/main.2cecd8eb.chunk.css"
+  },
+  {
+    "revision": "dc5e09bdd4daf95de143",
+    "url": "/static/js/2.d01b24e9.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.d01b24e9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c7ab8d91c43b0bc410d6",
+    "url": "/static/js/main.3086635f.chunk.js"
+  },
+  {
+    "revision": "16d50208a04408d1bf24",
+    "url": "/static/js/runtime-main.23041e79.js"
+  }
+]);
--- functions/index.js
+++ functions/index.js
@@ -5,8 +5,77 @@ const app = express();
 const path = require('path');
 const fs = require('fs');
 
+var admin = require("firebase-admin");
+const { TablesServiceClient } = require('@google/area120-tables');
+
+const client = new TablesServiceClient();
+
+var serviceAccount = require("./patrick-today-firebase-adminsdk-zeyvx-630f632f07.json");
+const { getPriority } = require('os');
+
+admin.initializeApp({
+    credential: admin.credential.cert(serviceAccount),
+    databaseURL: "https://patrick-today.firebaseio.com"
+});
+
+let db = admin.firestore();
+
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, '/public/home.html'));
 });
+
+function getIp(req) {
+    let i = (req.headers['x-forwarded-for'] || req.connection.remoteAddress) || 'error:'+Math.random()+':'+Date.now();
+    i = i.split(',')[0]
+    return i
+}
+
+app.get('/frame/connect', (req, res) => {
+    // res.send('hi')
+    let globalIp =  getIp(req);
+    // globalIp = 'poop'
+    if (globalIp.startsWith('undefined')) {
+        res.send("Couldn't find frame on your network. Try again soon.")
+    } else {
+        db.collection('frames').where("globalIp", "==", globalIp).get().then(snapshot => {
+            // console.log(snapshot);
+            let matches = []
+            snapshot.forEach(doc => {
+                matches.push(doc.data())
+            })
+            // res.send('matches')
+            if (matches.length == 0) {
+                res.status(200).send("Couldn't find frame on your network. Try again soon.")
+            } else if (matches.length == 1) {
+                res.redirect(matches[0].localIp)
+            } else {
+                let text = '';
+                matches.forEach(match => {
+                    text += `${match.localIp} <br>`
+                })
+                res.status(200).end(text)
+            }
+        })
+        .catch(() => {
+            res.status(500).send('error')
+        })
+    }
+    // res.send('hello')
+})
+
+app.get('/frame/set', (req, res) => {
+    // console.log(req);
+    let globalIp = getIp(req);
+    let localIp = req.query.ip || '';
+    let frameId = req.query.frameId || 'blank';
+    // res.send('globalip: '+globalIp)
+    db.collection('frames').doc(frameId).set({
+        globalIp, localIp, frameId
+    }).then(() => {
+        res.status(200).send('success');
+    }).catch(() => {
+        res.status(500).send('error');
+    })
+})
 
 exports.app = functions.https.onRequest(app);
